62d4646a43ec70edb27279945d6e1b32c3cadeda
Andrius Kairys <andrius.kairys.lt@gmail.com>
2021-05-09 16:12:47 +0300

    nasa api

--- js/ui.js
+++ js/ui.js
@@ -2,11 +2,26 @@ const numberGeneratorDOM = document.getElementById('numberGenerator');
 const numberDOM = document.getElementById('number');
 const button = document.getElementById('button');
 
-button.addEventListener('click', e => {
-    const number = Math.floor((Math.random() * 99) + 1);
-    numberDOM.innerText = number;
-    return;
-})
+const nasaPhotoDOM = document.getElementById('nasaPhoto');
+
+if (numberGeneratorDOM) {
+    button.addEventListener('click', e => {
+        const number = Math.floor((Math.random() * 99) + 1);
+        numberDOM.innerText = number;
+        return;
+    })
+}
+
+if (nasaPhotoDOM) {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+        nasaPhotoDOM.innerHTML = `<img src="${res.data.url}" alt="Nasa picture of the day">`;
+    })
+    .catch(err => console.log(err));
+}
+
+
+
 
 
 
