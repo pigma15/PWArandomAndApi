065b9b646c8e89611dc7cd8a3bf1d0ad14e13189
Andrius Kairys <andrius.kairys.lt@gmail.com>
2021-05-09 11:43:52 +0300

    fullscreen

--- sw.js
+++ sw.js
@@ -1,17 +1,32 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/css/style.js',
+    '/img/oldComputer.png'
+]
+
 //install service worker
 
 self.addEventListener('install', evt => {
-    console.log('installed', evt);
-})
+    //console.log('installed', evt);
+    caches.open(staticCacheName).then(cache => {
+        cache.addAll([
+
+        ]);
+    })
+});
 
 //activate event
 
 self.addEventListener('activate', evt => {
-    console.log('activated', evt);
-})
+    //console.log('activated', evt);
+});
 
 //fetch events
 
 self.addEventListener('fetch', evt => {
-    console.log('fetch', evt);
-})
+    //console.log('fetch', evt);
+});
